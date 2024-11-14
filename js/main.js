4a00ec1a3703bef43b12de0e5adbe213adf91018
Brodie Abrew <brodieabrew@u.boisestate.edu>
2024-11-14 13:45:52 -0700

    Template for adding ingredients

--- js/main.js
+++ js/main.js
@@ -1,18 +1,33 @@
-window.onload = loaded;
+const addIngredientButton = document.getElementById("add-ingredient");
+const recipeDataForm = document.getElementById("recipe-data");
+const ingredientTemplate = document.getElementById("ingredient-template");
+const recipeIngredients = document.getElementById("recipe-ingredients");
 
-/**
- * Simple Function that will be run when the browser is finished loading.
- */
-function loaded() {
-    // Assign to a variable so we can set a breakpoint in the debugger!
-    const hello = sayHello();
-    console.log(hello);
+let counter = 0;
+
+// Credits: https://stackoverflow.com/a/69362263 
+function addIngredient(event) {
+    event.preventDefault();
+
+    let div = document.createElement("div");
+    div.classList.add("ingredient");
+    div.appendChild(ingredientTemplate.content.cloneNode(true));
+
+    div.innerHTML = div.innerHTML.replaceAll("{i}", ++counter);
+    recipeIngredients.append(div);
 }
 
-/**
- * This function returns the string 'hello'
- * @return {string} the string hello
- */
-export function sayHello() {
-    return 'hello';
+function submitRecipe(event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+    const xhr = new XMLHttpRequest();
+
+    // TODO: Convert formData to JSON with proper arrays for ingredients
+    // Perhaps create an object to represent an ingredient
+    let d = Object.fromEntries(formData);
+    this.reset();
 }
+
+addIngredientButton.addEventListener("click", addIngredient);
+recipeDataForm.addEventListener("submit", submitRecipe);
